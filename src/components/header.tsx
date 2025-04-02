0bf3a784869c95bdf481d3bca3298ba2e540f769
Cléber <clebersevero1998@gmail.com>
2025-04-02 10:15:17 -0300

    fix: images imports

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,13 @@
+
+import Logo from "../assets/logo.png" // Adjust path accordingly
+
 const Header = () => {
   return (
     <div
       style={{ height: "200px" }}
       className="bg-todo-gray-700 flex d-flex justify-center items-center w-full"
     >
-      <img src="../src/assets/logo.png" />
+      <img src={Logo} />
     </div>
   );
 };
--- src/components/todo-list-placeholder.tsx
+++ src/components/todo-list-placeholder.tsx
@@ -1,8 +1,9 @@
+import ClipBoard from '../assets/Clipboard.png'
 
 const TodoListPlaceholder = () => {
   return (
     <div className="flex flex-col items-center pt-20 mt-4 border-t rounded-md border-todo-gray-400">
-      <img src="../src/assets/clipboard.png" className="mb-2"/>
+      <img src={ClipBoard} className="mb-2"/>
 
       <h3 className="font-bold text-todo-gray-300">
         Você ainda não tem tarefas cadastradas
